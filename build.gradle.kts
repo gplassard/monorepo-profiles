276ae497bd906e41eb16541348f4cd1a4337cc03
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2024-11-29 22:51:44 +0100

    chore(upgrade): add jackson deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
+    implementation(libs.jacksonCore)
+    implementation(libs.jacksonKotlin)
     implementation(libs.jacksonYaml)
     testImplementation(libs.junit)
 
